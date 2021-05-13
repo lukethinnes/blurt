b7a9234831e8ab96ad897af0d11ad0280d92bb78
Luke Thinnes <lukethinnes@yahoo.com>
2021-05-13 15:07:43 -0600

    Added React with framework and updated index to include Browser Router.

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function RestaurantsList() {
+  return (
+    <div className="App">
+      More Restaurants!
+    </div>
+  );
+}
+
+export default RestaurantsList;
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Restaurant() {
+  return (
+    <div className="App">
+      Restaurant
+    </div>
+  );
+}
+
+export default Restaurant;
