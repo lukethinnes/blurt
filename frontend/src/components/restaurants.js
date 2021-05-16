a4efa7048fbdef8a2e207ce8e4cf545746a58579
Luke Thinnes <lukethinnes@yahoo.com>
2021-05-15 20:28:25 -0600

    Built out the restaurant component.

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom'
 
+const Restaurant = props => {
+  const initialRestaurantState = {
+    id: null,
+    name: '',
+    address: {},
+    cuisine: '',
+    reviews: []
+  }
+  const [restaurant, setRestaurant] = useState(initialRestaurantState)
 
-function Restaurant() {
-  return (
-    <div className="App">
-      Restaurant
-    </div>
-  );
+  const getRestaurant = id => {
+    RestaurantDataService.get(id)
+      .then(response => {
+        setRestaurant(response.data)
+        console.log(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      }) 
+  }
+
+  useEffect(() => {
+    getRestaurant(props.match.params.id)
+  }, [props.match.params.id])
+
+  const deleteReview = (reviewId, index) => {
+    RestaurantDataService.deleteReview(reviewId)
+      .then(response => {
+        setRestaurant((prevState) => {
+          prevState.reviews.splice(index, 1)
+          return({
+            ...prevState
+          })
+        })
+        
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
+    return (
+      <div className='App'>
+        Hello World.
+      </div>
+    )
 }
 
 export default Restaurant;
