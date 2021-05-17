a2f1890f85b5c38a4d1e73570d2ace03a6a3ecdb
Luke Thinnes <lukethinnes@yahoo.com>
2021-05-17 16:23:23 -0600

    Built out login with state.

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const Login = props => {
+    const initialUserState = {
+        name: '',
+        id: ''
+    }
 
 
-function Login() {
-    return (
-        <div className="App">
-            Login:
-        </div>
-    );
+const [user, setUser] = useState(initialUserState)
+
+const handleInputChange = event => {
+    const { name, value } = event.target
+    setUser({ ...user, [name]: value})
 }
+
+const login = () => {
+    props.login(user)
+    props.history.push('/')
+}
+
+
+return (
+    <div className="submit form">
+        <div>
+            <div className='form-group'>
+                <label htmlFor='user'>Username</label>
+                <input
+                className='form-control'
+                id='name'
+                name='name'
+                onChange={handleInputChange}
+                required
+                type='text'
+                value={user.name}
+                />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor='id'>ID</label>
+                <input
+                className='form-control'
+                id='id'
+                name='id'
+                onChange={handleInputChange}
+                required
+                type='text'
+                value={user.id}
+                />
+            </div>
+
+            <button onClick={login} className='btn btn-success'/>
+        </div>
+    </div>
+);
+}
+
 
 export default Login;
--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import RestaurantDataService from "../services/restaurant";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom';
 
 const Restaurant = props => {
   const initialRestaurantState = {
     id: null,
-    name: "",
+    name: '',
     address: {},
-    cuisine: "",
+    cuisine: '',
     reviews: []
   };
   const [restaurant, setRestaurant] = useState(initialRestaurantState);
@@ -51,31 +51,31 @@ const Restaurant = props => {
             <strong>Cuisine: </strong>{restaurant.cuisine}<br/>
             <strong>Address: </strong>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}
           </p>
-          <Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-primary">
+          <Link to={'/restaurants/' + props.match.params.id + '/review'} className='btn btn-primary'>
             Add Review
           </Link>
           <h4> Reviews </h4>
-          <div className="row">
+          <div className='row'>
             {restaurant.reviews.length > 0 ? (
             restaurant.reviews.map((review, index) => {
               return (
-                <div className="col-lg-4 pb-1" key={index}>
-                  <div className="card">
-                    <div className="card-body">
-                      <p className="card-text">
+                <div className='col-lg-4 pb-1' key={index}>
+                  <div className='card'>
+                    <div className='card-body'>
+                      <p className='card-text'>
                         {review.text}<br/>
                         <strong>User: </strong>{review.name}<br/>
                         <strong>Date: </strong>{review.date}
                       </p>
                       {props.user && props.user.id === review.user_id &&
-                          <div className="row">
-                            <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
+                          <div className='row'>
+                            <a onClick={() => deleteReview(review._id, index)} className='btn btn-primary col-lg-5 mx-1 mb-1'>Delete</a>
                             <Link to={{
-                              pathname: "/restaurants/" + props.match.params.id + "/review",
+                              pathname: '/restaurants/' + props.match.params.id + '/review',
                               state: {
                                 currentReview: review
                               }
-                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
+                            }} className='btn btn-primary col-lg-5 mx-1 mb-1'>Edit</Link>
                           </div>                   
                       }
                     </div>
@@ -84,7 +84,7 @@ const Restaurant = props => {
               );
             })
             ) : (
-            <div className="col-sm-4">
+            <div className='col-sm-4'>
               <p>No reviews yet.</p>
             </div>
             )}
